3739352a5835726fcbf044f194e2ef8f6d883f86
StumpKeeper <alexeymkr@gmail.com>
2023-10-22 16:56:42 +0300

    Initial commit

---
+++ src/main/java/me/stumpkeeper/shitposter/ShitPosterApplication.java
@@ -0,0 +1,13 @@
+package me.stumpkeeper.shitposter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShitPosterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShitPosterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/stumpkeeper/shitposter/ShitPosterApplicationTests.java
@@ -0,0 +1,13 @@
+package me.stumpkeeper.shitposter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShitPosterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
